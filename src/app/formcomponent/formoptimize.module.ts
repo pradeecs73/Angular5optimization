7f8c508d24af847a7fedbb5f4e5fdd44b6beb0bc
pradeecs73 <pradeecs73@gmail.com>
2021-04-12 16:10:12 +0530

    removing / modifing form

--- src/app/formcomponent/formoptimize.module.ts
+++ src/app/formcomponent/formoptimize.module.ts
@@ -1,12 +1,19 @@
+import { Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormconfigModule } from './formconfig.module';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 import { FormcomponentComponent } from './formcomponent.component';
 
+const formRoutes: Routes = [
+  { path: '', component: FormcomponentComponent }
+];
+
+
 @NgModule({
   declarations: [FormcomponentComponent],
-  imports: [CommonModule, FormsModule, FormconfigModule]
+  imports: [CommonModule, FormsModule,RouterModule.forChild(formRoutes)],
+  exports: [RouterModule]
 })
 export class FormoptimizeModule {}
